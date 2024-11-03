a8397e09024b64cc98a38f9147257132c4d6e701
VladissGernet <skeimorr@gmail.com>
2024-11-03 17:58:52 +0400

    example

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { GlobalStyle } from './styles.js';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <p>App</p>
+    </>
+  );
+}
